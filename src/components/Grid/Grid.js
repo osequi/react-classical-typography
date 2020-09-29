8a9f9f0bbd63f36b4a87775f9f4625fd57f2e56f
osequi <cs@osequi.com>
2020-09-29 16:39:23 +0300

    The grid works

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/styles";
 import clsx from "clsx";
 import Helmet from "react-helmet";
+import shortid from "shortid";
 
 /**
  * Imports other components and hooks
@@ -14,6 +15,26 @@ import Helmet from "react-helmet";
 const propTypes = {
   fontSize: PropTypes.string,
   lineHeight: PropTypes.string,
+  /**
+   * Display horizontal lines?
+   */
+  displayHorizontalRhytm: PropTypes.bool,
+  /**
+   * Display vertica lines?
+   */
+  displayVerticalRhytm: PropTypes.bool,
+  /**
+   * Number of horizontal lines on the grid
+   */
+  numberOfHorizontalLines: PropTypes.number,
+  /**
+   * Number of vertical lines on the grid
+   */
+  numberOfVerticalLines: PropTypes.number,
+  /**
+   * The color of the grid line
+   */
+  lineColor: PropTypes.string,
 };
 
 /**
@@ -22,25 +43,141 @@ const propTypes = {
 const defaultProps = {
   fontSize: "100%",
   lineHeight: "1.25",
+  displayHorizontalRhytm: true,
+  displayVerticalRhytm: true,
+  numberOfHorizontalLines: 50,
+  numberOfVerticalLines: 100,
+  lineColor: "lightgrey",
 };
 
 /**
  * Defines the styles
  */
 const useStyles = makeStyles(() => ({
-  container: {},
+  rhythmContainer: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    top: 0,
+    left: 0,
+  },
+
+  verticalRhythmContainer: (props) => ({
+    display: props.displayVerticalRhytm ? "flex" : "none",
+    flexWrap: "wrap",
+  }),
+
+  horizontalRhythmContainer: (props) => ({
+    display: props.displayHorizontalRhytm ? "block" : "none",
+  }),
+
+  rhythmLine: (props) => ({
+    display: "block",
+    boxSizing: "border-box",
+    borderColor: props.lineColor ? props.lineColor : "transparent",
+  }),
+
+  verticalRhythmLine: {
+    width: "var(--lem)",
+    height: "100%",
+    borderRightStyle: "solid",
+  },
+
+  horizontalRhythmLine: {
+    width: "100%",
+    height: "var(--lem)",
+    borderBottomStyle: "solid",
+  },
 }));
+
+/**
+ * Sets up the typographic grid globally on the `<body>` tag.
+ * A bug in Helmet doesn't let setting up the `<body>` with object notation.
+ * @see https://github.com/nfl/react-helmet/issues/344
+ */
+const globalSettings = (props) => {
+  const { fontSize, lineHeight } = props;
+
+  return (
+    <Helmet>
+      <body
+        style={`font-size: ${fontSize}; line-height: ${lineHeight}; --lem: 1.25em`}
+      />
+    </Helmet>
+  );
+};
+
+/**
+ * Draws a set of lines
+ */
+const drawLines = (props) => {
+  const {
+    display,
+    numberOfLines,
+    container,
+    line,
+    rhythmContainer,
+    rhythmLine,
+  } = props;
+
+  if (!display) return null;
+
+  const lines = Array.from({ length: numberOfLines }, (v, i) => i).map(
+    (item) => {
+      return <span className={clsx("Line", line, rhythmLine)} />;
+    }
+  );
+
+  return (
+    <div className={clsx("Container", container, rhythmContainer)}>{lines}</div>
+  );
+};
 
 /**
  * Displays the component
  */
 const Grid = (props) => {
-  const { fontSize, lineHeight } = props;
+  const {
+    displayVerticalRhytm,
+    displayHorizontalRhytm,
+    numberOfHorizontalLines,
+    numberOfVerticalLines,
+    lineColor,
+  } = props;
+
+  const {
+    verticalRhythmContainer,
+    horizontalRhythmContainer,
+    verticalRhythmLine,
+    horizontalRhythmLine,
+    rhythmContainer,
+    rhythmLine,
+  } = useStyles(props);
+
+  const horizontalLines = drawLines({
+    display: displayHorizontalRhytm,
+    numberOfLines: numberOfHorizontalLines,
+    container: horizontalRhythmContainer,
+    line: horizontalRhythmLine,
+    rhythmContainer: rhythmContainer,
+    rhythmLine: rhythmLine,
+  });
+
+  const verticalLines = drawLines({
+    display: displayVerticalRhytm,
+    numberOfLines: numberOfVerticalLines,
+    container: verticalRhythmContainer,
+    line: verticalRhythmLine,
+    rhythmContainer: rhythmContainer,
+    rhythmLine: rhythmLine,
+  });
 
   return (
-    <Helmet>
-      <body style={`font-size: ${fontSize}; line-height: ${lineHeight}`} />
-    </Helmet>
+    <>
+      {globalSettings(props)}
+      {horizontalLines}
+      {verticalLines}
+    </>
   );
 };
 
